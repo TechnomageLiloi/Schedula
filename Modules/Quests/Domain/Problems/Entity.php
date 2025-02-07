e446d327eeab46652011a4d0f5c8cfc9f969108f
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-02-07 16:41:26 +0200

    [0.01.002] Add problems domain.

---
+++ Modules/Quests/Domain/Problems/Entity.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Liloi\Schedula\Modules\Quests\Domain\Problems;
+
+use Liloi\Stylo\Parser;
+use Liloi\Tools\Entity as AbstractEntity;
+
+/**
+ * @method string getTitle()
+ * @method void setTitle(string $value)
+ *
+ * @method string getStatus()
+ * @method void setStatus(string $value)
+ *
+ * @method string getProgram()
+ * @method void setProgram(string $value)
+ *
+ * @method string getData()
+ * @method void setData(string $value)
+ */
+class Entity extends AbstractEntity
+{
+    public function getKey(): string
+    {
+        return $this->getField('key_problem');
+    }
+
+    public function parse(): string
+    {
+        return Parser::parseString($this->getProgram());
+    }
+
+    public function save(): void
+    {
+        Manager::save($this);
+    }
+
+    public function getStatusTitle(): string
+    {
+        return Statuses::$list[$this->getStatus()];
+    }
+
+    public function getStatusClass(): string
+    {
+        return strtolower(str_replace(' ', '-', $this->getStatusTitle()));
+    }
+}
---
+++ Modules/Quests/Domain/Problems/Statuses.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Liloi\Schedula\Modules\Quests\Domain\Problems;
+
+/**
+ * Class diary types.
+ */
+class Statuses
+{
+    public const TODO = 1;
+    public const IN_HAND = 2;
+    public const SUCCESS = 3;
+    public const FAILURE = 4;
+
+    // @ToDo: To more abstract level with redefine.
+    static public array $list = [
+        self::TODO => 'To Do',
+        self::IN_HAND => 'In hand',
+        self::SUCCESS => 'Success',
+        self::FAILURE => 'Failure',
+    ];
+
+    // @todo: move this method to more abstract level.
+    public static function getClass(string $id): string
+    {
+        return strtolower(str_replace(' ', '-', self::$list[$id]));
+    }
+}
